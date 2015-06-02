2f906836181f4615e0f6dc0790f279adea2ed6c9
Jennifer Crawshaw <jennycrawshaw@gmail.com>
2015-06-02 12:23:50 -0400

    Add Player class and rendered canvas

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = 300;
 }
 
 // Update the enemy's position, required method for game
@@ -24,12 +26,33 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    //image for player character
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 425;
+}
 
+Player.prototype.update = function() {
+
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function() {
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var enemyOne = new Enemy;
+var enemyTwo = new Enemy;
+var enemyThree = new Enemy;
+var allEnemies = [enemyOne, enemyTwo, enemyThree];
+var player = new Player;
 
 
 // This listens for key presses and sends the keys to your
